2afb8f3886271cff5976b822bde5aa4f5f3c6ed0
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2026-05-20 13:51:51 +0800

    Remove dist/ from version control

--- dist/adapters/index.js
+++
@@ -1,8 +0,0 @@
-import { CpjourneyAdapter } from "./cpjourney-adapter.js";
-const ADAPTERS = {
-    CPJourney: CpjourneyAdapter,
-};
-export function createAdapter(name) {
-    return new ADAPTERS[name]();
-}
-//# sourceMappingURL=index.js.map
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { Client } from "./client.js";
-export { BaseAdapter } from "./adapters/base-adapter.js";
-//# sourceMappingURL=index.js.map

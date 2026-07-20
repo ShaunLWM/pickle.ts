e5c486a4fcae9bca2ec4ca78dbeec385e239b229
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2026-07-20 13:54:14 +0800

    chore: remove unsupported CPZero adapter

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -5,14 +5,12 @@ import {
 import type { BaseAdapter } from "./base-adapter.js";
 import { CpjourneyAdapter } from "./cpjourney-adapter.js";
 import { CplegacyAdapter } from "./cplegacy-adapter.js";
-import { CpzeroAdapter } from "./cpzero-adapter.js";
 import { NewcpAdapter } from "./newcp-adapter.js";
 import { PenguinoriginsAdapter } from "./penguinorigins-adapter.js";
 
 const ADAPTERS = {
   CPJourney: CpjourneyAdapter,
   CPLegacy: CplegacyAdapter,
-  CPZero: CpzeroAdapter,
   NewCP: NewcpAdapter,
   PenguinOrigins: PenguinoriginsAdapter,
 } as const;
